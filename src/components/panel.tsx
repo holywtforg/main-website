2cd8bef4778ff70789a085e5f3566c83eaa7e001
sweethuman <gheo@sweethuman.tech>
2021-07-30 17:44:25 +0300

    feat: add illustrations

--- src/components/panel.tsx
+++ src/components/panel.tsx
@@ -8,7 +8,7 @@ const Panel: React.FC<Props> = ({ title, children }) => {
     <div className="flex justify-center items-center min-h-screen">
       <div className="flex-col justify-center items-center space-y-5 text-primary w-3/4 xl:w-1/2 m-auto">
         <div className="font-display text-center text-7xl">{title}</div>
-        <div className="bg-secondary px-16 py-24 rounded-lg text-white text-5xl">{children}</div>
+        <div className="bg-secondary px-16 py-24 rounded-lg text-white text-5xl relative">{children}</div>
       </div>
     </div>
   );
