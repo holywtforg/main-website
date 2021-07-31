01a4e0ee923b5324e69d773fb0a62877e5306c2f
sweethuman <gheo@sweethuman.tech>
2021-07-31 17:13:06 +0300

    refactor: query images via gatsby image

--- src/components/teammate.tsx
+++ src/components/teammate.tsx
@@ -1,19 +1,18 @@
 import * as React from "react";
+import { GatsbyImage } from "gatsby-plugin-image";
+import { IGatsbyImageData } from "gatsby-plugin-image/dist/src/components/gatsby-image.browser";
 
 type Props = {
   name: string;
   role: string;
-  image: string;
+  image: IGatsbyImageData;
   url: string;
 };
 
 const Teammate: React.FC<Props> = ({ name, role, image, url }) => {
   return (
     <div className="flex justify-start items-center font-serif text-xl md:text-2xl lg:text-3xl">
-      <div
-        className="h-28 w-28 rounded-full bg-cover bg-center bg-origin-border bg-clip-border m-4"
-        style={{ backgroundImage: `url(${image})` }}
-      />
+      <GatsbyImage image={image} alt={name} className="h-28 w-28 rounded-full m-4" />
       <div>
         <div
           className="cursor-pointer transition duration-500 ease-in-out transform origin-left hover:-translate-y-1 hover:scale-110"
