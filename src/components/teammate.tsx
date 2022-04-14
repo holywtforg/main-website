910926b110b39b567c6554d2ccc804dd72d722b9
sweethuman <gheo@sweethuman.tech>
2022-04-14 20:24:35 +0300

    feat(team): make full profile clickable

--- src/components/teammate.tsx
+++ src/components/teammate.tsx
@@ -11,14 +11,12 @@ type Props = {
 
 const Teammate: React.FC<Props> = ({ name, role, image, url }) => {
   return (
-    <div className="flex justify-start items-center font-serif text-xl md:text-2xl lg:text-3xl">
+    <div
+      className="flex justify-start items-center font-serif text-xl md:text-2xl lg:text-3xl cursor-pointer transition duration-500 ease-in-out transform origin-left hover:-translate-y-1 hover:scale-110"
+      onClick={() => window.open(url, "_blank")}>
       <GatsbyImage image={image} alt={name} className="h-28 w-28 rounded-full m-4" />
       <div>
-        <div
-          className="cursor-pointer transition duration-500 ease-in-out transform origin-left hover:-translate-y-1 hover:scale-110"
-          onClick={() => window.open(url, "_blank")}>
-          {name}
-        </div>
+        <div>{name}</div>
         <div>
           Role: <span className="text-tertiary">{role}</span>
         </div>
